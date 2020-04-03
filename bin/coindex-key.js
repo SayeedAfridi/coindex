7a0a3e1a0efba65cbdf4d569d912f883f195fd27
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-04 03:13:01 +0600

    structured key command with actions in different file

--- bin/coindex-key.js
+++ bin/coindex-key.js
@@ -1,22 +1,10 @@
 const program = require("commander");
-
+const key = require("../commands/key");
 program
     .command("set")
     .description("Set API Key -- Get at https://nomics.com")
-    .action(() => {
-        console.log("Hello from set");
-    });
-program
-    .command("show")
-    .description("Show API Key")
-    .action(() => {
-        console.log("Hello from show");
-    });
-program
-    .command("remove")
-    .description("Remove API Key")
-    .action(() => {
-        console.log("Hello from remove");
-    });
+    .action(key.set);
+program.command("show").description("Show API Key").action(key.show);
+program.command("remove").description("Remove API Key").action(key.remove);
 
 program.parse(process.argv);
