0df19aca085945b80bc709ca0d7fc42b6040b7e1
Sayeed Afridi <as.afridi563@gmail.com>
2020-04-04 04:16:13 +0600

    added price function for check price command

---
+++ commands/check.js
@@ -0,0 +1,18 @@
+const colors = require("colors");
+const KeyManager = require("../lib/KeyManager");
+const CryptoAPI = require("../lib/CryptoAPI");
+const check = {
+    async price(cmd) {
+        try {
+            const keyManager = new KeyManager();
+            const key = keyManager.getKey();
+            const api = new CryptoAPI(key);
+            const priceData = await api.getPriceData(cmd.coin, cmd.cur);
+            console.log(priceData);
+        } catch (err) {
+            console.error(err.message.red);
+        }
+    },
+};
+
+module.exports = check;
